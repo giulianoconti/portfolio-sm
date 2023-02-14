94a24a883cb2a84e22a4f19a73bf59ca44ba0fd8
GiuliannT <giuliconti1@gmail.com>
2023-02-14 12:37:17 -0300

    cleaning and improving code

--- code.js
+++ code.js
@@ -10,57 +10,53 @@ const projectsSection = document.querySelector("#projects");
 const contactSection = document.querySelector("#contact");
 const menu = document.querySelector(".nav_list");
 const projectsList = document.querySelector(".projects_list");
+const projectsButtonsContainer = document.querySelector(".projects_buttons_container");
 const contactForm = document.querySelector(".contact_form");
 let prevOrNextSectionRunning = false;
 let showMenu = false;
+let isMobile = false;
 
-/* navbar item active */
 const activeMenu = () => {
-  const scrollY = window.pageYOffset;
-  if (scrollY < aboutSection.offsetTop - aboutSection.offsetTop / 4) {
+  const scrollY = window.scrollY;
+  if (scrollY < aboutSection.offsetTop - aboutSection.offsetTop / 3) {
     removeAllActive();
     homeMenu.classList.add("active");
-    return;
-  } else if (scrollY < projectsSection.offsetTop - projectsSection.offsetTop / 4) {
+  } else if (scrollY < projectsSection.offsetTop - projectsSection.offsetTop / 3) {
     removeAllActive();
     aboutMenu.classList.add("active");
-    return;
-  } else if (scrollY < contactSection.offsetTop - contactSection.offsetTop / 4) {
+  } else if (scrollY < contactSection.offsetTop - contactSection.offsetTop / 4.5) {
     removeAllActive();
     projectsMenu.classList.add("active");
-    return;
   } else {
     removeAllActive();
     contactMenu.classList.add("active");
-    return;
   }
 };
-window.addEventListener("scroll", activeMenu);
 
 const removeAllActive = () => {
-  homeMenu.classList.remove("active");
-  aboutMenu.classList.remove("active");
-  projectsMenu.classList.remove("active");
-  contactMenu.classList.remove("active");
+  // This function removes the active class from all menu items
+  const menuItems = document.querySelectorAll(".nav_link");
+  menuItems.forEach((item) => item.classList.remove("active"));
 };
 
-/* navbar menu btn toggle */
 const toggleMenu = () => {
-  if (window.innerWidth <= 790) {
-    if (!showMenu) {
-      menu.classList.add("nav_list_active");
-      menuBtn.classList.add("d_none");
-      showMenu = true;
-    } else {
-      menu.classList.remove("nav_list_active");
-      menuBtn.classList.remove("d_none");
-      showMenu = false;
-    }
+  // If window is less than 790px wide, and menu is not shown, show menu
+  if (window.innerWidth <= 790 && !showMenu) {
+    menu.classList.add("nav_list_active");
+    menuBtn.classList.add("d_none");
+    showMenu = true;
+  } else {
+    // Otherwise, hide menu
+    menu.classList.remove("nav_list_active");
+    menuBtn.classList.remove("d_none");
+    showMenu = false;
   }
 };
 
 const removeMenu = () => {
+  // if menu is shown and the window is narrow
   if (showMenu && window.innerWidth <= 790) {
+    // hide the menu
     menu.classList.remove("nav_list_active");
     menuBtn.classList.remove("d_none");
     showMenu = false;
@@ -68,66 +64,61 @@ const removeMenu = () => {
 };
 
 const handleMyAge = () => {
+  // Create date objects for today and my birthday
   const today = new Date();
   const birthDate = new Date("2001-03-07");
+
+  // Calculate my age
   let age = today.getFullYear() - birthDate.getFullYear();
   const m = today.getMonth() - birthDate.getMonth();
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     age--;
   }
+
+  // Display my age
   myAge.innerHTML = age;
 };
 
-handleMyAge();
-
-/* Projects - Section 3 */
-const handlePrev = () => {
+const handleNextPrev = (type) => {
   if (!prevOrNextSectionRunning) {
     prevOrNextSectionRunning = true;
-    if (projectsList.scrollLeft === 0) {
-      projectsList.scrollLeft = projectsList.scrollWidth - projectsList.clientWidth;
-      setTimeout(() => {
-        prevOrNextSectionRunning = false;
-      }, 800);
+    console.log(projectsList.clientWidth);
+    if (type === "prev") {
+      // If user is on the first project, scroll to the last project
+      if (projectsList.scrollLeft === 0) {
+        projectsList.scrollLeft = projectsList.scrollWidth - projectsList.clientWidth;
+      } else {
+        projectsList.scrollLeft -= projectsList.clientWidth;
+      }
     } else {
-      projectsList.scrollLeft -= projectsList.clientWidth;
-      setTimeout(() => {
-        prevOrNextSectionRunning = false;
-      }, 500);
+      // If user is on the last project, scroll to the first project
+      if (projectsList.scrollWidth - projectsList.scrollLeft === projectsList.clientWidth) {
+        projectsList.scrollLeft = 0;
+      } else {
+        projectsList.scrollLeft += projectsList.clientWidth;
+      }
     }
+    setTimeout(
+      () => {
+        prevOrNextSectionRunning = false;
+      },
+      (projectsList.scrollLeft === 0 && type === "prev") ||
+        (projectsList.scrollWidth - projectsList.scrollLeft === projectsList.clientWidth &&
+          type === "next")
+        ? 900
+        : 500
+    );
   }
 };
 
-const handleNext = () => {
-  if (!prevOrNextSectionRunning) {
-    prevOrNextSectionRunning = true;
-    if (projectsList.scrollWidth - projectsList.scrollLeft === projectsList.clientWidth) {
-      projectsList.scrollLeft = 0;
-      setTimeout(() => {
-        prevOrNextSectionRunning = false;
-      }, 800);
-    } else {
-      projectsList.scrollLeft += projectsList.clientWidth;
-      setTimeout(() => {
-        prevOrNextSectionRunning = false;
-      }, 500);
-    }
-  }
-};
-
-/* if resize */
-window.addEventListener("resize", () => {
-  projectsList.scrollLeft = 0;
-});
-
-/* fetch projects */
-fetch("https://giuliannt.github.io/api/myProjects.json")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((project) => {
-      const projectDiv = document.createElement("div");
-      projectDiv.className = "project";
-      projectDiv.innerHTML = `
+const fetchProjects = () => {
+  fetch("https://giuliannt.github.io/api/myProjects.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((project) => {
+        const projectDiv = document.createElement("div");
+        projectDiv.className = "project";
+        projectDiv.innerHTML = `
       <img class="project_img" src="${project.img}" alt="${project.title}"></img>
       <div class="project_info">
         <h3 class="project_title">${project.title}</h3>
@@ -152,11 +143,57 @@ fetch("https://giuliannt.github.io/api/myProjects.json")
         </div>
       </div>
     `;
-      projectsList.appendChild(projectDiv);
+        projectsList.appendChild(projectDiv);
+      });
     });
-  });
+};
 
-  // reset contact form on page refresh
-  window.onbeforeunload = () => {
-    contactForm.reset();
+const isMobileOrNot = () => {
+  if (mobileAndTabletCheck()) {
+    projectsList.classList.add("projects_list_mobile");
+    projectsButtonsContainer.classList.add("d_none");
+  } else if (projectsList.classList.contains("projects_list_mobile")) {
+    projectsList.classList.remove("projects_list_mobile");
+    projectsButtonsContainer.classList.remove("d_none");
   }
+};
+
+window.mobileAndTabletCheck = function () {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
+};
+
+window.addEventListener("scroll", activeMenu);
+
+window.addEventListener("resize", () => {
+  // reset the scroll position
+  projectsList.scrollLeft = 0;
+  // detect if the device is mobile
+  isMobileOrNot();
+});
+
+window.onbeforeunload = () => {
+  // reset contact form on page refresh
+  contactForm.reset();
+};
+
+handleMyAge();
+
+fetchProjects();
+
+isMobileOrNot();
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/sw.js");
+}
