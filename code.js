3a0b776992ae23e4fb5b202fdb949933ab8c924d
GiuliannT <giuliconti1@gmail.com>
2023-02-23 23:06:27 -0300

    redirect removed

--- code.js
+++ code.js
@@ -11,9 +11,13 @@ const contactSection = document.querySelector("#contact");
 const menu = document.querySelector(".nav_list");
 const projectsList = document.querySelector(".projects_list");
 const projectsButtonsContainer = document.querySelector(".projects_buttons_container");
+const contactForm = document.querySelector(".contact_form");
+const contactStatus = document.querySelector(".contact_status");
 let prevOrNextSectionRunning = false;
 let showMenu = false;
 let isMobile = false;
+let windowWidth = window.innerWidth;
+// consts for change language
 const navbarHome = document.querySelector(".navbar_home");
 const navbarAbout = document.querySelector(".navbar_about");
 const navbarProjects = document.querySelector(".navbar_projects");
@@ -30,7 +34,7 @@ const projectsTitle = document.querySelector(".projects_title");
 const contactInput = document.querySelector(".contact_input");
 const contactMessage = document.querySelector(".contact_message");
 const contactTextarea = document.querySelector(".contact_textarea");
-let windowWidth = window.innerWidth;
+let currentLanguage = "es";
 
 const languages = {
   en: {
@@ -60,6 +64,8 @@ const languages = {
       emailInput: "example@gmail.com",
       message: "Message",
       messageInput: "Hello, I would like to contact you for...",
+      statusTextOk: "Thanks for your submission!",
+      statusTextError: "Oops! There was a problem submitting your form.",
     },
   },
   es: {
@@ -89,6 +95,8 @@ const languages = {
       emailInput: "ejemplo@gmail.com",
       message: "Mensaje:",
       messageInput: "Buenas, me gustaría contactar contigo para...",
+      statusTextOk: "¡Gracias por tu envío!",
+      statusTextError: "¡Ups! Hubo un problema al enviar tu formulario.",
     },
   },
   pt: {
@@ -118,6 +126,8 @@ const languages = {
       emailInput: "exemplo@gmail.com",
       message: "Mensagem:",
       messageInput: "Olá, gostaria de entrar em contato com você para...",
+      statusTextOk: "Obrigado pelo seu envio!",
+      statusTextError: "Ops! Houve um problema ao enviar seu formulário.",
     },
   },
 };
@@ -170,6 +180,8 @@ const removeMenu = () => {
 };
 
 const handleLang = (lang = "es") => {
+  // This function changes the language of the page
+  currentLanguage = lang;
   fetchProjects(lang);
   navbarHome.textContent = languages[lang].navbar.home;
   navbarAbout.textContent = languages[lang].navbar.about;
@@ -270,6 +282,35 @@ const fetchProjects = async (lang = "es") => {
   });
 };
 
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  fetch(e.target.action, {
+    method: contactForm.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        contactStatus.innerHTML = languages[currentLanguage].contact.statusTextOk;
+        contactForm.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            contactStatus.innerHTML = data["errors"].map((error) => error["message"]).join(", ");
+          } else {
+            contactStatus.innerHTML = languages[currentLanguage].contact.statusTextError;
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      contactStatus.innerHTML = languages[currentLanguage].contact.statusTextError;
+    });
+};
+
 const isMobileOrNot = () => {
   if (mobileAndTabletCheck()) {
     projectsList.classList.add("projects_list_mobile");
@@ -280,21 +321,7 @@ const isMobileOrNot = () => {
   }
 };
 
-window.mobileAndTabletCheck = function () {
-  let check = false;
-  (function (a) {
-    if (
-      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
-        a
-      ) ||
-      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-        a.substr(0, 4)
-      )
-    )
-      check = true;
-  })(navigator.userAgent || navigator.vendor || window.opera);
-  return check;
-};
+contactForm.addEventListener("submit", handleSubmit);
 
 window.addEventListener("scroll", activeMenu);
 
@@ -310,11 +337,20 @@ window.addEventListener("resize", () => {
   }
 });
 
-window.onunload = window.onbeforeunload = () => {
-  // Reset contact form on page refresh
-  for (const form of document.getElementsByTagName("form")) {
-    form.reset();
-  }
+window.mobileAndTabletCheck = function () {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
 };
 
 handleMyAge();
