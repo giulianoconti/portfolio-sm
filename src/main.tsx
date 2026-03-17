b060d2dc5b1f0047f424f6ea57d562c4fcde97ee
giulianoconti <giuliconti1@gmail.com>
2026-03-17 16:33:41 -0300

    redirects

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,19 @@ import "./index.css";
 import { LocaleProvider } from "./contexts/LocaleContext.tsx";
 import App from "./App.tsx";
 
+const pathToLang: Record<string, string> = {
+  "/es.html": "es",
+  "/pt.html": "pt",
+  "/en.html": "en",
+};
+const lang = pathToLang[window.location.pathname];
+if (lang) {
+  const url = new URL(window.location.href);
+  url.pathname = "/";
+  url.searchParams.set("lang", lang);
+  window.location.replace(url.pathname + url.search + url.hash);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LocaleProvider>
